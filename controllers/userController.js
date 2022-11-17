df7b678f89986ded90e3f8f7013002dc95bddff4
bs23.hasan.ali <hasan.haolader@brainstation-23.com>
2022-11-18 00:05:12 +0600

    feature/search & reset done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,37 @@
 const usersModel = require("../models/usersModel");
 
 class userController {
-    static async index(req, res, next){
-        const {page} = req.query;
+    static async index(req, res, next) {
+        const page = req.query.page;
+        const query = {};
+        if (req.query.search) {
+            query.username = req.query.search;
+        }
         const options = {
             page: parseInt(page, 10) || 1,
-            limit: 10
+            limit: 1
         }
-
-        usersModel.paginate({}, options).then((results, err) => {
-            if(!err){
-            //Pass the totalpages number to pug along with the result
-            console.log();
-                res.render('audit-log', {testObj: results.docs, page_count: results.totalPages})
-                res.render('admin/user/index.pug', {users: results.docs, paging_counter: results.pagingCounter, limit: results.limit, total_docs: results.totalDocs ,page_count: results.totalPages, current_page:results.page, 'title': 'Users List'});
+        usersModel.paginate(query, options).then((results, err) => {
+            if (!err) {
+                //Pass the total pages number to pug along with the result
+                res.render('audit-log', {
+                    testObj: results.docs,
+                    page_count: results.totalPages
+                })
+                res.render('admin/user/index.pug', {
+                    users: results.docs,
+                    paging_counter: results.pagingCounter,
+                    limit: results.limit,
+                    total_docs: results.totalDocs,
+                    page_count: results.totalPages,
+                    current_page: results.page,
+                    'title': 'Users List',
+                    reqData: req.query
+                });
 
             }
         })
-
-
-        const users = await  usersModel.find();
+        const users = await usersModel.find();
     }
 }
 module.exports = userController;
